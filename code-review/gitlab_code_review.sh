967d7020e51b053779805cbcb54e471ccc3dc9de
Addison <addison.emig@mrs-electronics.com>
2025-07-25 08:47:36 -0400

    feat: gitlab code review script

---
+++ code-review/gitlab_code_review.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# The following environment variables must be included in the
+# GitLab job definition:
+#    - OPENROUTER_KEY
+#    - GITLAB_TOKEN
+# The others are automatically included in GitLab merge request pipelines.
+
+# Authenticate with GitLab
+glab auth login --token $GITLAB_TOKEN
+
+# Collect all the context
+mkdir -p .bots/context
+# Collect the merge request details
+glab mr view $CI_MERGE_REQUEST_IID > .bots/context/merge-request.md
+# Collect the diffs
+glab mr diff $CI_MERGE_REQUEST_IID > .bots/context/diffs.md
+# TODO: include merge request comments in the context
+
+# Combine context into a single `.bots/context.md` file
+for f in .bots/context/*; do
+    echo -e "\n===== BEGIN FILE: $f =====\n"; cat "$f";
+done > .bots/context.md
+
+# Generate the LLM review
+# TODO: move the summary to a separate multi-line variable
+cat .bots/context.md | llm -m summary-model -s "Summarize this merge request. Please note any concerns in the following areas: security, performances, and best practices. For each concern, please include at least one possible solution." > .bots/summary.md
+
+# Leave the comment
+# NOTE: The "|| true" is because `glab mr note` has an unhandled error,
+#       even when the comment posts successfully
+glab mr note $CI_MERGE_REQUEST_IID -m "$(cat .bots/summary.md)" || true
