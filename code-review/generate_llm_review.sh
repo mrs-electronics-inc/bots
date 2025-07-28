7394a00e86d9b327f4205f29a832f3c1dbd8f34f
Addison <addison.emig@mrs-electronics.com>
2025-07-28 13:52:38 -0400

    feat: don't use model alias

--- code-review/generate_llm_review.sh
+++ code-review/generate_llm_review.sh
@@ -3,6 +3,8 @@
 # This script generates the LLM review.
 # It expects the .bots/context.md file to exist.
 
+SUMMARY_MODEL=openrouter/google/gemini-2.5-flash-lite
+
 # Generate the LLM review
 # TODO: move the summary to a separate multi-line variable
-cat .bots/context.md | llm -m summary-model -s "Summarize this pull/merge request. Please note any concerns in the following areas: security, performances, and best practices. For each concern, please include at least one possible solution." > .bots/summary.md
+cat .bots/context.md | llm -m $SUMMARY_MODEL -s "Summarize this pull/merge request. Please note any concerns in the following areas: security, performances, and best practices. For each concern, please include at least one possible solution." > .bots/summary.md
--- code-review/github_code_review.sh
+++ code-review/github_code_review.sh
@@ -8,20 +8,12 @@
 # Trust the current directory
 git config --global --add safe.directory "$(pwd)"
 
-echo "Hello world!"
-
-gh --version
-
 # Collect all the context
 export PLATFORM="github"
 collect_context.sh
 
-echo "Context collected!"
-
 # Generate the LLM review
 generate_llm_review.sh
-
-echo "Code review generated!"
 
 # Leave the comment
 gh pr comment $GITHUB_HEAD_REF -F .bots/summary.md
