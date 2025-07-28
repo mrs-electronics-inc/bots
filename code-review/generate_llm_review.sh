db324c77892747d2e2e3b310ab6029984b3ad7af
Addison <addison.emig@mrs-electronics.com>
2025-07-28 14:09:33 -0400

    debug

--- code-review/generate_llm_review.sh
+++ code-review/generate_llm_review.sh
@@ -10,6 +10,10 @@ SUMMARY_MODEL=openrouter/google/gemini-2.5-flash-lite
 # https://github.com/simonw/llm-openrouter/issues/34
 llm keys set openrouter --value OPENROUTER_KEY
 
+echo $OPENROUTER_KEY
+
+llm models
+
 # Generate the LLM review
 # TODO: move the summary to a separate multi-line variable
 cat .bots/context.md | llm -m $SUMMARY_MODEL -s "Summarize this pull/merge request. Please note any concerns in the following areas: security, performances, and best practices. For each concern, please include at least one possible solution." > .bots/summary.md
