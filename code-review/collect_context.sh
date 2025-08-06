732a7b84f7bf8e3b0e0da26e2c44cebae2791243
Addison <addison.emig@mrs-electronics.com>
2025-08-06 06:35:33 -0400

    fix: write to comments

--- code-review/collect_context.sh
+++ code-review/collect_context.sh
@@ -25,8 +25,7 @@ elif [ "$PLATFORM" == "github" ]; then
     # Collect the pull request details
     gh pr view $GITHUB_HEAD_REF > .bots/context/details
     # Collect the pull request comments
-    gh api "repos/$GITHUB_REPOSITORY/issues/$PULL_REQUEST_NUMBER/comments" | jq '.[] | {username: .user.login, timestamp: .created_at, body: .body, id: .id}'
-     > .bots/context/comments
+    gh api "repos/$GITHUB_REPOSITORY/issues/$PULL_REQUEST_NUMBER/comments" | jq '.[] | {username: .user.login, timestamp: .created_at, body: .body, id: .id}' > .bots/context/comments
     # Collect the diffs
     gh pr diff $GITHUB_HEAD_REF > .bots/context/diffs
     # Collect the names of the changed files
