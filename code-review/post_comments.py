4ce35ec90410692598c963cbb0390aece7dc4cdd
Addison <addison.emig@mrs-electronics.com>
2025-09-04 14:17:39 -0400

    docs: update comment

--- code-review/post_comments.py
+++ code-review/post_comments.py
@@ -115,15 +115,15 @@ def post_gitlab_comments(main_comment, change_requests):
                     'base_sha': mr.diff_refs['base_sha'],
                     'start_sha': mr.diff_refs['start_sha'],
                     'head_sha': mr.diff_refs['head_sha'],
-                    # 'old_path': change_request['old_file_path'],
                     'new_path': change_request['new_file_path'],
-                    # 'old_line': change_request['old_start_line_number'],
                     'new_line': change_request['new_start_line_number'],
                     # TODO(#42): re-enable this once we figure out how to
-                    #       fix the line_code calculation
+                    #            fix the line_code calculation
                     # It is possible the GitLab API is broke
                     # https://gitlab.com/gitlab-org/gitlab/-/issues/524665
                     # https://gitlab.com/gitlab-org/gitlab/-/issues/520794
+                    # 'old_path': change_request['old_file_path'],
+                    # 'old_line': change_request['old_start_line_number'],
                     # 'line_range': generate_gitlab_line_range(change_request)
                 }
             }
