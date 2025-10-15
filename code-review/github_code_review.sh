b733c14db87458c27f45a7dae742624091e4d820
Addison <addison.emig@mrs-electronics.com>
2025-10-15 09:15:05 -0400

    fix: remove references to /repo

--- code-review/github_code_review.sh
+++ code-review/github_code_review.sh
@@ -7,10 +7,6 @@
 # The others are automatically automatically included in GitHub pull request pipelines.
 set -e
 
-# I don't know why we need this when it is already in the Dockerfile
-# But the GitHub workflow fails otherwise.
-git config --global --add safe.directory /repo
-
 export PLATFORM="github"
 
 # Generate the LLM review
